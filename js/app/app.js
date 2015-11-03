0092efa533b58ebb633af938e724cb6c693b0702
rafael silva <rafaelsilva1406@gmail.com>
2015-11-03 08:06:24 -0800

    Added logic for options params not all params are require check readme file for further notes

--- js/app/app.js
+++ js/app/app.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 	$('input[name="color"]').colorBar({
-		parentEl:{}
+		parentElAttr:{'class':'test1'},
+		leftInnerElAttr:{'class':'test2'},
+		rightInnerElAttr:{'class':'test3'},
+		unOrderElAttr:{'class':'test4'},
+		listElAttr:{'class':'test5'},
+		inputElAttr:{'class':'test6'},
+		buttonElAttr:{'class':'test7'},
+		colorArr:['red','blue','white','black'],
+		buttonText:'Verify Color',
+		position:'bottom'
 	});
 });
--- js/components/jquery/colorBar.js
+++ js/components/jquery/colorBar.js
@@ -2,41 +2,109 @@
 * @file Working with input
 * @author Rafael Silva
 * @version 0.1
+* @Summary: API below creates markup template and add ability to attach attributes for further customization.
 */
 
 (function ($){
 	function colorBarException(message) {
-		return "colorBar API Exception" + message;
+		return "colorBar API Exception: " + message;
 	}
 
 	$.fn.colorBar = function(options){
-		var $this = $(this),
-			p = document.createElement('div'),
-			lc = document.createElement('div'),
-			rc = document.createElement('div'),
-			cb = document.createElement('ul'),
-			btn = document.createElement('button');
+		var $this = $(this), //store instance input locally
+			p = document.createElement('div'), //parent div
+			lc = document.createElement('div'), //left child div
+			rc = document.createElement('div'), //right child div
+			cb = document.createElement('ul'), //colorBar el
+			btn = document.createElement('button'), //button
+			coutColors = 0; //keep count of colors
 
-		//check
+		//check if object not empty
 		if(!$.isEmptyObject(options)){
-			if($.isEmptyObject(options.parentEl)){
-				//create api defaults
-			}else{
-
+			//attach attributes
+			if(!$.isEmptyObject(options.inputElAttr)){
+				$(this).prop(options.inputElAttr);
 			}
-		}else{
-			//set defaults
-			var apiSettings = $.extend({
 
-			},options);
+			//attach attributes
+			if(!$.isEmptyObject(options.parentElAttr)){
+				$(p).prop(options.parentElAttr);
+			}
+
+			//attach attributes
+			if(!$.isEmptyObject(options.leftInnerElAttr)){
+				$(lc).prop(options.leftInnerElAttr);
+			}
+
+			//attach attributes
+			if(!$.isEmptyObject(options.rightInnerElAttr)){
+				$(rc).prop(options.rightInnerElAttr);
+			}
+
+			//attach attributes
+			if(!$.isEmptyObject(options.unOrderElAttr)){
+				$(cb).prop(options.unOrderElAttr);
+			}
+
+			//attach attributes
+			if(!$.isEmptyObject(options.buttonElAttr)){
+				$(btn).prop(options.buttonElAttr);
+			}
+
+			//attach text node
+			if(!$.isEmptyObject(options.buttonText)){
+				var btnText = document.createTextNode(options.buttonText);
+				btn.appendChild(btnText);
+			}else{
+				//throw ex empty string name for button
+				throw new Error(colorBarException('Must pass string name for buttton'));
+			}
+
+			//build markup
+			$this.wrap($(p));
+			$this.wrap($(lc));
+			$(rc).insertAfter($this.parent());
+			$this.parent().parent().find('div:nth-child(2)').prepend($(btn));
+
+			//set ColorBar position depending on value
+			if(!$.isEmptyObject(options.position)){
+				switch($.trim(options.position).toLowerCase()){
+					case 'top'://set list on top of input
+						$(cb).insertBefore($this);
+						break;
+					case 'bottom'://set list below input
+						$(cb).insertAfter($this);
+						break;
+				}
+			}else{
+				//throw ex empty string name for button
+				throw new Error(colorBarException('Must pass position for unorder list'));
+			}
+
+			//set list of colors
+			if(options.colorArr.length != 0){
+				$.each(options.colorArr , function(i,v){//go through the list of color and do stufff
+					coutColors++;//start counting array
+					var li = document.createElement('li'),//create li for each item
+						t = document.createTextNode(coutColors); //set count as text value
+					li.style.backgroundColor = v;//set user value color as background
+					//attach attributes
+					if(!$.isEmptyObject(options.listElAttr)){
+						$(li).prop(options.listElAttr);
+					}
+					li.appendChild(t);//append text to li
+					cb.appendChild(li);//append li to ul
+				});
+			}else{
+				//throw ex empty list
+				throw new Error(colorBarException('Color values must be empty'));
+			}
+
+		}else{
+			//throw ex params options can not be empty
+			throw new Error(colorBarException('Please look at API document some params must be passed to have API work properly'));
 		}
 
-		$this.wrap($(p));
-		$this.wrap($(lc));
-		console.log(cb);
-		$(cb).insertBefore($this);
-		$(rc).insertAfter($this.parent());
-		$this.parent().parent().find('div:nth-child(2)').prepend($(btn));
 		return $this;
 	};
 }(jQuery));
